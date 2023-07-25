730fb98ae4b7289f802d27e7ee65066d7bbb5226
Gokul <amkrishna2483@gmail.com>
2023-07-25 16:30:29 +0530

    html input

--- src/App.js
+++ src/App.js
@@ -16,41 +16,41 @@ function App() {
         <h2>It's my toDo   ☕ </h2>
       </div>
       <div className="input">
-        <input value={toDo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={()=>setTodos([...toDos,{time: Date.now(), text:toDo, status: false}])} className="fas fa-plus"></i>
+        <input value={toDo} onChange={(e) => setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={() => setTodos([...toDos, { time: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
         {
-          toDos.map((obj)=>{
+          toDos.map((obj) => {
             return (<div className="todo">
-            <div className="left">
-              <input onChange={(e)=>{
-                setTodos(toDos.filter(obj2=>{
-                  if(obj2.time === obj.time){
-                    obj2.status = e.target.checked;
-                  } return obj2;
-                }))
-                console.log(e.target.checked);
-                console.log(obj);
-              }} value={obj.status} type="checkbox" name="" id="" />
-              <p>{obj.text}</p>
-            </div>
-            <div className="right">
-              <i onClick={()=>{
-                setTodos(toDos.filter((obj2)=>{
-                  return obj2.time !== obj.time;
-                }))
-              }} className="fas fa-times"></i>
-            </div>
-          </div>)
+              <div className="left">
+                <input onChange={(e) => {
+                  setTodos(toDos.filter(obj2 => {
+                    if (obj2.time === obj.time) {
+                      obj2.status = e.target.checked;
+                    } return obj2;
+                  }))
+                  console.log(e.target.checked);
+                  console.log(obj);
+                }} value={obj.status} type="checkbox" name="" id="" />
+                <p>{obj.text}</p>
+              </div>
+              <div className="right">
+                <i onClick={() => {
+                  setTodos(toDos.filter((obj2) => {
+                    return obj2.time !== obj.time;
+                  }))
+                }} className="fas fa-times"></i>
+              </div>
+            </div>)
           })}
-          <br />
-          <div style={{color:'white'}}><h2>Completed List</h2></div>
-          {
-            toDos.map((obj)=>{
-              return obj.status? (<h2 style={{color:'white'}}> -> {obj.text}</h2>) : null;
-            })
-          }
+        <br />
+        <div style={{ color: 'white' }}><h2>Completed List</h2></div>
+        {
+          toDos.map((obj) => {
+            return obj.status ? (<h2 style={{ color: 'white' }}> -&gt; {obj.text}</h2>) : null;
+          })
+        }
       </div>
     </div>
   )
